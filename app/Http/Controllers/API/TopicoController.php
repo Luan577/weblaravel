d3028875ea166e3d75b68eefd760e9b08a30f9c0
Alanakcb <alanacantuaria@gmail.com>
2022-08-30 12:21:25 -0300

    errro no sen mensagem

--- app/Http/Controllers/API/TopicoController.php
+++ app/Http/Controllers/API/TopicoController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\Topico;
-use app\Traits\ApiResponse;
+use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
 
 class TopicoController extends Controller
